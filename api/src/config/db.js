c98e4784fb11d24f1e15950d5c4fc6df935b8a19
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2025-12-17 02:01:56 +0300

    config files

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -3,11 +3,18 @@ import { MONGO_URI } from "./env.js";
 
 export default async function connectDB() {
 	try {
-		await mongoose.connect(MONGO_URI);
+		await mongoose.connect(MONGO_URI)
 		console.log("DB Connected successfully!");
+		// mongoose.connection.once("open", async () => {
+		// 	console.log("DB:", mongoose.connection.name);
+		// 	const cols = await mongoose.connection.db.listCollections().toArray();
+		// 	console.log(
+		// 		"Collections:",
+		// 		cols.map((c) => c.name)
+		// 	);
+		// });
 	} catch (error) {
 		console.log(error.message);
 		process.exit(1);
 	}
 }
-
--- api/src/config/env.js
+++ api/src/config/env.js
@@ -5,5 +5,5 @@ dotenv.config();
 export const PORT = process.env.PORT;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const MONGO_URI = process.env.MONGO_URI;
+export const POSTGRES_URI = process.env.POSTGRES_URI;
 export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
-
