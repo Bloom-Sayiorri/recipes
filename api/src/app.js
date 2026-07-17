c9f21236e580dcaa9ca51690e48aff25ba59dea9
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2026-07-18 00:22:31 +0300

    testing routes

--- api/src/app.js
+++ api/src/app.js
@@ -5,14 +5,14 @@ import cors from "cors";
 
 import errorMiddleware from "./middlewares/error.middleware.js";
 
-import userRouter from "./routes/user.route.js";
-import authRouter from "./routes/auth.route.js";
-import recipeRouter from "./routes/recipe.route.js";
-import reviewRouter from "./routes/review.route.js";
-import favoriteRouter from "./routes/favorite.route.js";
-import notificationRouter from "./routes/notification.route.js";
-import profileRouter from "./routes/profile.route.js";
-import contactRouter from "./routes/contact.route.js";
+// import userRouter from "./routes/user.route.js";
+// import authRouter from "./routes/auth.route.js";
+// import recipeRouter from "./routes/recipe.route.js";
+// import reviewRouter from "./routes/review.route.js";
+// import favoriteRouter from "./routes/favorite.route.js";
+// import notificationRouter from "./routes/notification.route.js";
+// import profileRouter from "./routes/profile.route.js";
+// import contactRouter from "./routes/contact.route.js";
 
 const app = express();
 
@@ -24,6 +24,7 @@ app.use(
 		allowedHeaders: ["Content-Type", "Authorization"],
 	})
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -45,16 +46,15 @@ app.use(async (req, res, next) => {
 	}
 });
 
-app.use("/api/users", userRouter);
-app.use("/api/auth", authRouter);
-app.use("/api/recipes", recipeRouter);
-app.use("/api/reviews", reviewRouter);
-app.use("/api/favorites", favoriteRouter);
-app.use("/api/notifications", notificationRouter);
-app.use("/api/profiles", profileRouter);
-app.use("/api/contact", contactRouter);
+// app.use("/api/users", userRouter);
+// app.use("/api/auth", authRouter);
+// app.use("/api/recipes", recipeRouter);
+// app.use("/api/reviews", reviewRouter);
+// app.use("/api/favorites", favoriteRouter);
+// app.use("/api/notifications", notificationRouter);
+// app.use("/api/profiles", profileRouter);
+// app.use("/api/contact", contactRouter);
 
 app.use(errorMiddleware);
-
 
 export default app;
