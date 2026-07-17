32b15caed73a8a50d4246bbb82c693c98b7dffe3
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2026-07-18 00:10:14 +0300

    import error fixed

--- api/src/app.js
+++ api/src/app.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ["https://recipiez-share.netlify.app/", "http://localhost:4001"],
+		origin: ["https://recipiez-share.netlify.app", "http://localhost:4001"],
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
 		allowedHeaders: ["Content-Type", "Authorization"],
